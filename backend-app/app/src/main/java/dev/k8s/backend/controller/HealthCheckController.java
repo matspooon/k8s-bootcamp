33188793b724e7e6f1ac6695ccce12c1b334fdc3
solsin <solasin@gmail.com>
2025-09-25 22:08:51 +0900

    HealthCheck 추가

---
+++ backend-app/app/src/main/java/dev/k8s/backend/controller/HealthCheckController.java
@@ -0,0 +1,20 @@
+package dev.k8s.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import dev.k8s.backend.common.VersionProvider;
+
+@RestController
+public class HealthCheckController {
+    private final VersionProvider versionProvider;
+
+    public HealthCheckController(VersionProvider versionProvider) {
+        this.versionProvider = versionProvider;
+    }
+
+    @GetMapping("/healthcheck")
+    public String hello() {
+        return versionProvider.getVersion().toString();
+    }
+}
